850fe05a6cde428c4d6d2e6301f9d6076e275473
david <ddvital0@gmail.com>
2021-02-23 06:00:21 -0300

    fixing

--- books/static/js/script.js
+++ books/static/js/script.js
@@ -13,26 +13,7 @@ function addMobileMenu() {
     if (window.innerWidth <= 600) {
         if (mobileMenu === null) {
 
-            header.innerHTML += `
-                <nav class="mobile-menu">
-                    <a href="{% url 'index' %}" class="mobile-menu-items {% if request.path == index %}active{% endif %}">
-                        <i class="fas fa-home"></i>
-                        <span>Home</span>
-                    </a>
-                    <a href="{% url 'books' %}" class="mobile-menu-items {% if request.path == books %}active{% endif %}">
-                        <i class="fas fa-books"></i>
-                        <span>Books</span>
-                    </a>
-                    <a href="{% url 'want_to_read' %}" class="mobile-menu-items {% if request.path == want_to_read %}active{% endif %}">
-                        <i class="fas fa-list"></i>
-                        <span>Wishlist</span>
-                    </a>
-                    <a href="{% url 'profile' %}" class="mobile-menu-items {% if request.path == profile %}active{% endif %}">
-                        <i class="fas fa-user"></i>
-                        <span>user</span>
-                    </a>
-                </nav>
-            `; 
+            header.innerHTML += mobileMenuContent 
         }
     } else {
         if (mobileMenu) {
